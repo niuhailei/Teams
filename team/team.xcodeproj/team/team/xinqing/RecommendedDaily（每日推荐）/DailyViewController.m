072d284c769ad58fab11bdaf830ba92fcd264a1b
lanou3g <18833886059@163.com>
2015-11-11 21:33:44 +0800

    11.11-21:32扫一扫（待定实现），每日推荐完成（数据请求和界面）----钮海雷

--- team/team.xcodeproj/team/team/xinqing/RecommendedDaily（每日推荐）/DailyViewController.m
+++ team/team.xcodeproj/team/team/xinqing/RecommendedDaily（每日推荐）/DailyViewController.m
@@ -10,14 +10,100 @@
 
 @interface DailyViewController ()
 
+@property(nonatomic,strong)ModelForMeiwen *model;
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (weak, nonatomic) IBOutlet UIView *dailyView;
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *authorLabel;
+
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+
+
 @end
 
 @implementation DailyViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    //关闭滑动反弹
+    self.scrollView.bounces = NO;
+    self.scrollView.scrollEnabled = NO;
+    
+    self.textView.bounces = NO;
+    
+    //设置各个视图透明颜色
+    self.scrollView.backgroundColor = [UIColor clearColor];
+    self.dailyView.backgroundColor = [UIColor clearColor];
+    self.textView.backgroundColor = [UIColor clearColor];
+    
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self requerAllDatasWithString:nil];
+    
+    
+    
+    NSLog(@"aaa");
 }
+
+
+//请求数据
+-(void)requerAllDatasWithString:(NSString *)string{
+    
+    //字符串拼接，根据日期，拼接成不同网址
+    string = [NSString stringWithFormat:@"%d",20151111];
+    
+    NSString *stringUrl = [kHTTPMEIRIYIWEN stringByAppendingString:string];
+    NSURL *url = [[NSURL alloc] initWithString:stringUrl];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+        
+        self.model = [[ModelForMeiwen alloc] init];
+        [self.model setValuesForKeysWithDictionary:dict];
+        
+        self.titleLabel.text = self.model.title;
+        self.authorLabel.text = self.model.author;
+        self.textView.text = self.model.content;
+        
+        //关闭整页滑动
+        self.textView.pagingEnabled = NO;
+        //关闭中央回弹
+        self.textView.bouncesZoom = NO;
+        
+        NSLog(@"%@",self.model.title);
+
+        
+        
+        
+        
+    }];
+        
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
